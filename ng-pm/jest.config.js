97226ec335df3b9f05a247c16713b63e28f305af
Patrick Ineichen <patrick.ineichen@bbv.ch>
2023-01-18 11:37:05 +0100

    add jest.config.js

--- ng-pm/jest.config.js
+++ ng-pm/jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   preset: 'jest-preset-angular',
   roots: [''],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageReporters: ['html', 'text', 'cobertura'],
+  coverageDirectory: 'dist/reports/coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
   testPathIgnorePatterns: ['node_modules', 'dist', 'cypress'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/'
